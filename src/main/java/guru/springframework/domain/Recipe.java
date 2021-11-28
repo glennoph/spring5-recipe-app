273d23718f2b423cfdae93094a916f5614b10e46
glenn opdycke-hansen <glennoph@gmail.com>
2021-11-28 12:59:41 -0600

    Many to many Category - Recipe fix join table name

--- src/main/java/guru/springframework/domain/Recipe.java
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -32,7 +32,7 @@ public class Recipe {
     private Notes notes;
 
     @ManyToMany
-    @JoinTable(name = "recipe-category",
+    @JoinTable(name = "recipe_category",
             joinColumns = @JoinColumn(name = "recipe_id") ,
             inverseJoinColumns = @JoinColumn(name = "category_id")  )
     private Set<Category> categories;
