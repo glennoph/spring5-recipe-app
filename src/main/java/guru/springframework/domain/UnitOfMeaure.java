8e0de64d6076da5b57610158f575e36ac12d325a
glenn opdycke-hansen <glennoph@gmail.com>
2022-02-20 14:41:47 -0600

    update UnitOfMeaure

--- src/main/java/guru/springframework/domain/UnitOfMeaure.java
+++ src/main/java/guru/springframework/domain/UnitOfMeaure.java
@@ -1,6 +1,9 @@
 package guru.springframework.domain;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class UnitOfMeaure {
@@ -9,8 +12,8 @@ public class UnitOfMeaure {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @OneToOne
-    private Ingredient ingredient;
+    private String description;
+
 
     public Long getId() {
         return id;
@@ -20,23 +23,11 @@ public class UnitOfMeaure {
         this.id = id;
     }
 
-    public Ingredient getIngredient() {
-        return ingredient;
+    public String getDescription() {
+        return description;
     }
 
-    public void setIngredient(Ingredient ingredient) {
-        this.ingredient = ingredient;
+    public void setDescription(String description) {
+        this.description = description;
     }
-
-    public String getUom() {
-        return uom;
-    }
-
-    public void setUom(String uom) {
-        this.uom = uom;
-    }
-
-    private String uom;
-
-
 }
