0d83d640b4bbbea54e5441a4d9e1bc393b3ba829
glenn opdycke-hansen <glennoph@gmail.com>
2022-02-27 16:17:01 -0600

    add lombok

--- src/main/java/guru/springframework/domain/Recipe.java
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -118,6 +118,7 @@ public class Recipe {
 
     public void setNotes(Notes notes) {
         this.notes = notes;
+        notes.setRecipe(this);
     }
 
     public Set<Ingredient> getIngredients() {
@@ -127,6 +128,13 @@ public class Recipe {
     public void setIngredients(Set<Ingredient> ingredients) {
         this.ingredients = ingredients;
     }
+
+    public Recipe addIngredient(Ingredient ingredient) {
+        ingredient.setRecipe(this);
+        this.ingredients.add(ingredient);
+        return this;
+    }
+
 
     public Difficulty getDifficulty() {
         return difficulty;
